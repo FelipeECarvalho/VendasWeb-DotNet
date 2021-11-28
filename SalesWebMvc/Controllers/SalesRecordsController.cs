07420aa026fa20e89df40dc9daefebddf3a3808d
FelipeECarvalho <Felipeemanuelc12@gmail.com>
2021-11-28 18:39:34 -0300

    Implementando a busca simples

---
+++ SalesWebMvc/Controllers/SalesRecordsController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SalesWebMvc.Controllers
+{
+	public class SalesRecordsController : Controller
+	{
+		private readonly SalesRecordService _salesService;
+
+		public SalesRecordsController(SalesRecordService salesService)
+		{
+			_salesService = salesService;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public async Task<IActionResult> SimpleSearchAsync(DateTime? initialDate, DateTime? finalDate)
+		{
+			if (!initialDate.HasValue) 
+			{
+				initialDate = new DateTime(DateTime.Now.Year, 1, 1);
+			}
+			if (!finalDate.HasValue)
+			{
+				finalDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, DateTime.Now.Day);
+			}
+			ViewData["initialDate"] = initialDate.Value.ToString("yyyy-MM-dd");
+			ViewData["finalDate"] = finalDate.Value.ToString("yyyy-MM-dd");
+			var result = await _salesService.FindByDateAsync(initialDate, finalDate);
+			return View(result);
+		}
+
+		public IActionResult GroupingSearch()
+		{
+			return View();
+		}
+
+
+	}
+}
---
+++ SalesWebMvc/Services/SalesRecordService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesWebMvc.Services
+{
+	public class SalesRecordService
+	{
+		private readonly SalesWebMvcContext _context;
+
+		public SalesRecordService(SalesWebMvcContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<SalesRecord>> FindByDateAsync(DateTime? initialDate, DateTime? finalDate) 
+		{
+			var result = from sales in _context.SalesRecord select sales;
+
+			if (initialDate.HasValue)
+			{
+				result = result.Where(x => x.Date >= initialDate);
+			}
+
+			if (finalDate.HasValue) 
+			{
+				result = result.Where(x => x.Date <= finalDate);
+			}
+
+			return await result
+				.Include(x => x.Seller)
+				.Include(x => x.Seller.Department)
+				.OrderByDescending(x => x.Date)
+				.ToListAsync();
+		}
+	}
+}
